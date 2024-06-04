06babae49b20b8fe19a203fe769066816b9dcdbe
Muskaan <109030675+mahajanmuskaan@users.noreply.github.com>
2024-06-04 15:48:07 +0530

    Delete src directory

--- src/main/java/com/springboot/electronicstore/repositories/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.electronicstore.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.electronicstore.models.CartItem;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Integer>{
-
-}
--- src/main/java/com/springboot/electronicstore/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.electronicstore.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.electronicstore.models.Product;
-
-public interface ProductRepository extends JpaRepository<Product, String>{
-
-}
